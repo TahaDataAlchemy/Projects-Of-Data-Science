08a4bf076c875b14c0aba25df821593613acd92b
TahaDataAlchemy <tahamehboob281@gmail.com>
2024-05-24 19:16:58 +0500

    Cached = False

--- MLOPS PROJECT/pipelines/training_pipeline.py
+++ MLOPS PROJECT/pipelines/training_pipeline.py
@@ -4,7 +4,9 @@ from steps.Evalution import evaluate_model
 from steps.data_ingestion import ingest_df
 from steps.model_train import train_model
 
-@pipeline
+@pipeline(enable_cache=False)
+#CACHE FALSE KA FAIDA YE HAI K AB AGAR CODE ME KOI CHANGES NHI HAI OR DATA ME KOI CHANGES NHI HAI TU WO PEECHLI WALI EXECUTION KO REFER KARKE CHALE GA
+
 
 def train_pipeline(data_path: str):
     df = ingest_df(data_path)
--- MLOPS PROJECT/run_pipeline.py
+++ MLOPS PROJECT/run_pipeline.py
@@ -3,3 +3,5 @@ from pipelines.training_pipeline import train_pipeline
 if __name__ == "__main__":
     train_pipeline(data_path = r"C:\Users\Pc\Desktop\Taha\Projects-Of-Data-Science\MLOPS PROJECT\data\olist_customers_dataset.csv")
 
+
+# 1:15:00
