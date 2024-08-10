ea39fde0c2c14392ca4444e701703d9dec6a3277
TahaDataAlchemy <157484236+TahaDataAlchemy@users.noreply.github.com>
2024-08-10 15:57:16 +0500

    Update app.py

--- END TO END MLOPS PROJECT/app.py
+++ END TO END MLOPS PROJECT/app.py
@@ -7,7 +7,6 @@ import sys
 
 
 
-
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
 sys.path.append(project_root)
 from sklearn.preprocessing import StandardScaler
